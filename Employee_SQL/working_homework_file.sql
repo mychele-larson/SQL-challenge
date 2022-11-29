3eb9d8a3c517416d461b29f46e54c471508c5efb
Mychele Larson <110074895+mychele-larson@users.noreply.github.com>
2022-11-28 19:59:03 -0600

    updated homework file

--- Employee_SQL/working_homework_file.sql
+++ Employee_SQL/working_homework_file.sql
@@ -17,15 +17,53 @@ AND hire_date >= '01/01/1986';
 -- List the manager name and dept number, dept name, employee number, last name & first name.
 
 SELECT dept_manager.emp_no,
-		dept_manager.dept_no,
 		departments.dept_name,
 		departments.dept_no,
 		employees.emp_no,
 		employees.last_name,
 		employees.first_name
-FROM departments
-JOIN dept_manager.dept_no ON
+FROM dept_manager
+LEFT JOIN departments ON
+dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
+
+
+-- List the dept number, employee number, last name, first name and department name for each employee
+
+SELECT employees.emp_no, 
+		employees.last_name, 
+		employees.first_name, 
+		departments.dept_name,
+		dept_manager.dept_no
+FROM employees
+RIGHT JOIN dept_manager ON
+employees.emp_no = dept_manager.emp_no
+RIGHT JOIN departments ON
 dept_manager.dept_no = departments.dept_no;
 
+-- List first name, last name and gender of employee with first name "Hercules" and last name begins with "B"
 
--- List the dept number for each employee
+SELECT * FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%'
+AND sex IS NOT NULL;
+
+-- List all employees in Sales Department-including employee number, first name and last name
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp ON
+e.emp_no = dept_emp.emp_no
+INNER JOIN departments AS d ON
+dept_emp.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR
+		dept_name = 'Development';
+		
+		
+-- List frequency counts (descending order) of all employee last names
+
+SELECT last_name, COUNT(last_name) FROM employees
+GROUP BY last_name
+ORDER BY count(last_name) desc;
+		
